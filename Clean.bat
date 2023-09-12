dc732e9c61028a879d7fe24c7307141b6bfc7a68
WorkChaosen <132773460+WorkChaosen@users.noreply.github.com>
2023-09-12 14:45:49 +0800

    Update Clean.bat

--- Clean.bat
+++ Clean.bat
@@ -3,6 +3,14 @@ cls
 
 echo Please wait...
 
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo This script needs to be run with administrator privileges.
+    echo Please right-click on the script and select "Run as administrator".
+    pause
+    exit
+)
+
 set "keyword=B_B_Q_"
 
 for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
